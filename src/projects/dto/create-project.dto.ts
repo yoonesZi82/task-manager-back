5dfd2bbfa6ba8351954f18242820adb9720e7af9
yoones <yooneszaamni58@gmail.com>
2025-07-21 11:38:40 +0330

    end project

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -2,11 +2,15 @@ import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
 import projectStatusEnum from '../enums/projectStatusEnum';
 
 export class CreateProjectDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Project name is required' })
+  @IsString({
+    message: 'Project name must be a string and is can not be number',
+  })
   name: string;
 
-  @IsEnum(projectStatusEnum)
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Project status is required' })
+  @IsEnum(projectStatusEnum, {
+    message: `Project status must be a valid status (${projectStatusEnum.ENABLED}, ${projectStatusEnum.DISABLED})`,
+  })
   status: projectStatusEnum;
 }
